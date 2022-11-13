7da02928d8e0053d30c286ef16c982a289d20191
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-13 20:49:04 +0500

    10 misol

--- basic_func10.py
+++ basic_func10.py
@@ -1,7 +1,7 @@
 # Create a function called main.
 def main(a):
 # Create function arguments a.
-    value =a + -4
+    value = a-8
 # Return the negative value of a.
     return value
-print(main(2))
+print (main(7))
