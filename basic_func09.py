01c4765aa9612416c54168460c522ceec3fde892
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-15 15:10:31 +0500

    9 misol

--- basic_func09.py
+++ basic_func09.py
@@ -1,7 +1,7 @@
 # Create a function called main.
 def main(a):
 # Create function arguments a.
-    value = a+5
+    value = a-1
 # decrease the value of a to one and return
     return value
-print(main(1))
+print(main(5))
