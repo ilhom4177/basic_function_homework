79a962f1a38e726c78f5a17033ecede8c190076b
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-15 15:12:53 +0500

    8 misol

--- basic_func08.py
+++ basic_func08.py
@@ -1,7 +1,7 @@
 # Create a function called main.
 def main(a):
 # Create function arguments a.
-    value =a+2
+    value =a+1
     return value
 # Increase the value of a to one and return.
 print(main(1))
